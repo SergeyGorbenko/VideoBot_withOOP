6708fbbb70c3ce78cd107a758310ce00cd56fb20
sergor13 <32778801+SergeyGorbenko@users.noreply.github.com>
2019-03-14 18:11:05 +0200

    Delete constants.py

--- bot/constants.py
+++
@@ -1,9 +0,0 @@
-token = "492444477:AAHedxtiBZCSg4AeXDk93zCayFQ5cGDDq3Y"
-gorbenko = 417755355
-rumsha = 233100225
-youtube = 'https://www.youtube.com/watch?v='
-admins = '..\\data\\.admins'
-videos_list = '..\\data\\list.files'
-priority_list = '..\\data\\priority'
-videos = '..\\Video\\'
-data = '..\\data\\'
